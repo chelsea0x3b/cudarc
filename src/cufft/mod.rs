04f219cf1ad8845a5aebd9dec516abcaec5c1041
Chelsea Lowman <clowman1993@gmail.com>
2025-12-11 15:51:11 +0000

    Feature guard cufft & cutensor

--- src/cufft/mod.rs
+++ src/cufft/mod.rs
@@ -1,3 +1,11 @@
+#![cfg(not(any(
+    feature = "cuda-11040",
+    feature = "cuda-11050",
+    feature = "cuda-11060",
+    feature = "cuda-11070",
+    feature = "cuda-11080",
+)))]
+
 pub mod result;
 #[allow(warnings)]
 pub mod sys;
